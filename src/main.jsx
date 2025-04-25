4f44e3e1aa2c98df25e563c29f3dd7031f49d301
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-04-25 14:47:00 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
